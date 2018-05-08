23e08d7770291d170b05369948e6689f5c074d48
JeremyTCD <jeremytanchongdao@gmail.com>
2018-05-08 18:19:58 +0800

    Fixed minor issues in SectionParser, cleaned up comments

--- JeremyTCD.Markdig.Extensions/Sections/SectionParser.cs
+++ JeremyTCD.Markdig.Extensions/Sections/SectionParser.cs
@@ -42,15 +42,22 @@ namespace JeremyTCD.Markdig.Extensions
 
         public override BlockState TryContinue(BlockProcessor processor, Block block)
         {
+            // If first non-whitespace char is not #, continue
+            if(processor.CurrentChar != _headingBlockParser.OpeningCharacters[0])
+            {
+                return BlockState.Continue;
+            }
+
             BlockState headingBlockState = _headingBlockParser.TryOpen(processor);
 
+            // Current line is not a heading block
             if (headingBlockState == BlockState.None)
             {
                 return BlockState.Continue;
             }
 
-            // Section followed by heading in OpenedBlocks
-            if(processor.NextContinue is HeadingBlock)
+            // Blocks can only be added to the last open block, close any open children of the section block
+            if(processor.NextContinue != null) // TODO is it ever null?
             {
                 processor.Close(processor.NextContinue);
             }
@@ -58,7 +65,7 @@ namespace JeremyTCD.Markdig.Extensions
             HeadingBlock newHeadingBlock = processor.NewBlocks.Peek() as HeadingBlock;
             SectionBlock currentSectionBlock = block as SectionBlock;
 
-            // TODO if levels are skipped, e.g 2 to 4
+            // Nest if new heading block has higher level than current section block
             if(newHeadingBlock.Level > currentSectionBlock.Level)
             {
                 var sectionBlock = new SectionBlock(this) { Level = newHeadingBlock.Level };
@@ -69,9 +76,11 @@ namespace JeremyTCD.Markdig.Extensions
                 return BlockState.Continue;
             }
 
-            // Equal or lower level, remove new heading block, close current section (ListBlockParser does somthing similar for thematic breaks)
+            // Equal or lower level, remove new heading block (ListBlockParser does somthing similar for thematic breaks)
             processor.NewBlocks.Pop();
+            processor.GoToColumn(0);
 
+            // Close current section
             return BlockState.None;
         }
     }
