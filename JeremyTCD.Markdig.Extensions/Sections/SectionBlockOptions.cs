7847656d23625c25dfc27e6773a86208ed43646a
JeremyTCD <jeremytanchongdao@gmail.com>
2018-05-16 22:29:17 +0800

    Added comments for SectionBlockOptions

--- JeremyTCD.Markdig.Extensions/Sections/SectionBlockOptions.cs
+++ JeremyTCD.Markdig.Extensions/Sections/SectionBlockOptions.cs
@@ -5,14 +5,34 @@ namespace JeremyTCD.Markdig.Extensions.Sections
 {
     public class SectionBlockOptions : IMarkdownObjectOptions<SectionBlockOptions>
     {
+        /// <summary>
+        /// <see cref="IdentifierService"/>
+        /// </summary>
         public bool GenerateIdentifier { get; set; } = true;
+
+        /// <summary>
+        /// <see cref="AutoLinkService"/>
+        /// </summary>
         public bool AutoLinkable { get; set; } = true;
+
+        /// <summary>
+        /// Used if section's level is 1.
+        /// </summary>
         public SectioningContentElement Level1WrapperElement { get; set; } = SectioningContentElement.None;
+
+        /// <summary>
+        /// Used if section's level is greater than or equal to 2.
+        /// </summary>
         public SectioningContentElement Level2PlusWrapperElement { get; set; } = SectioningContentElement.Section;
 
-        // TODO test attribute population and overwriting
+        /// <summary>
+        /// HTML attributes
+        /// </summary>
         public Dictionary<string, string> Attributes { get; set; } = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 
+        /// <summary>
+        /// Returns a deep clone.
+        /// </summary>
         public SectionBlockOptions Clone()
         {
             SectionBlockOptions result = MemberwiseClone() as SectionBlockOptions;
