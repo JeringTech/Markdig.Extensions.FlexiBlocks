72600b5849c066ab8b536998d919e403fe9bb2f9
JeremyTCD <jeremytanchongdao@gmail.com>
2018-05-23 21:36:22 +0800

    Cleaned up HeadingBlockRenderer, added tests

---
+++ JeremyTCD.Markdig.Extensions.Tests/Sections/HeadingBlockRendererIntegrationTests.cs
@@ -0,0 +1,75 @@
+﻿using JeremyTCD.Markdig.Extensions.Sections;
+using Markdig.Renderers;
+using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace JeremyTCD.Markdig.Extensions.Tests.Sections
+{
+    public class HeadingBlockRendererIntegrationTests
+    {
+        [Theory]
+        [MemberData(nameof(Write_WritesHeaderAndHeading_Data))]
+        public void Write_WritesHeaderAndHeading(string dummyChildText, int dummyLevel, string expectedResult)
+        {
+            // Arrange
+            var dummyContainerInline = new ContainerInline();
+            dummyContainerInline.AppendChild(new LiteralInline(dummyChildText));
+            var dummyHeadingBlock = new HeadingBlock(null) { Level = dummyLevel, Inline = dummyContainerInline };
+            var headingBlockRenderer = new HeadingBlockRenderer();
+
+            string result = null;
+            using (var stringWriter = new StringWriter())
+            {
+                var dummyHtmlRenderer = new HtmlRenderer(stringWriter);
+
+                // Act
+                headingBlockRenderer.Write(dummyHtmlRenderer, dummyHeadingBlock);
+                result = stringWriter.ToString();
+            }
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> Write_WritesHeaderAndHeading_Data()
+        {
+            const string dummyChildText = "dummyChildText";
+
+            return new object[][]
+            {
+                new object[]{ dummyChildText, 1, $"<header class=\"header-level-1\">\n<h1>{dummyChildText}</h1>\n</header>\n"},
+                new object[]{ dummyChildText, 6, $"<header class=\"header-level-6\">\n<h6>{dummyChildText}</h6>\n</header>\n"}
+            };
+        }
+
+        [Fact]
+        public void Write_WritesIconMarkupIfSpecified()
+        {
+            // Arrange
+            const string dummyIconMarkup = "dummyIconMarkup";
+            const string dummyChildText = "dummyChildText";
+            const int dummyLevel = 1;
+            var dummyContainerInline = new ContainerInline();
+            dummyContainerInline.AppendChild(new LiteralInline(dummyChildText));
+            var dummyHeadingBlock = new HeadingBlock(null) { Level = dummyLevel, Inline = dummyContainerInline };
+            dummyHeadingBlock.SetData(SectionBlockParser.ICON_MARKUP_KEY, dummyIconMarkup);
+            var headingBlockRenderer = new HeadingBlockRenderer();
+
+            string result = null;
+            using (var stringWriter = new StringWriter())
+            {
+                var dummyHtmlRenderer = new HtmlRenderer(stringWriter);
+
+                // Act
+                headingBlockRenderer.Write(dummyHtmlRenderer, dummyHeadingBlock);
+                result = stringWriter.ToString();
+            }
+
+            // Assert
+            Assert.Equal($"<header class=\"header-level-{dummyLevel}\">\n<h{dummyLevel}>{dummyChildText}</h{dummyLevel}>\n{dummyIconMarkup}\n</header>\n", result);
+        }
+    }
+}
--- JeremyTCD.Markdig.Extensions/Sections/HeadingBlockRenderer.cs
+++ JeremyTCD.Markdig.Extensions/Sections/HeadingBlockRenderer.cs
@@ -1,7 +1,6 @@
 ﻿using Markdig.Renderers;
 using Markdig.Renderers.Html;
 using Markdig.Syntax;
-using System.Globalization;
 
 namespace JeremyTCD.Markdig.Extensions.Sections
 {
@@ -10,33 +9,19 @@ namespace JeremyTCD.Markdig.Extensions.Sections
     /// </summary>
     public class HeadingBlockRenderer : HtmlObjectRenderer<HeadingBlock>
     {
-        private static readonly string[] HeadingTexts = {
-            "h1",
-            "h2",
-            "h3",
-            "h4",
-            "h5",
-            "h6",
-        };
-
         protected override void Write(HtmlRenderer renderer, HeadingBlock obj)
         {
-            string headingText = obj.Level > 0 && obj.Level <= 6
-                ? HeadingTexts[obj.Level - 1]
-                : "<h" + obj.Level.ToString(CultureInfo.InvariantCulture);
-
             if (renderer.EnableHtmlForBlock)
             {
-                // Wrap heading in a header element - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/header
                 renderer.WriteLine($"<header class=\"header-level-{obj.Level}\">");
-                renderer.Write("<").Write(headingText).WriteAttributes(obj).Write(">");
+                renderer.Write($"<h{obj.Level}>");
             }
 
             renderer.WriteLeafInline(obj);
 
             if (renderer.EnableHtmlForBlock)
             {
-                renderer.Write("</").Write(headingText).WriteLine(">");
+                renderer.WriteLine($"</h{obj.Level}>");
 
                 if (renderer.EnableHtmlForBlock && obj.GetData(SectionBlockParser.ICON_MARKUP_KEY) is string iconMarkup)
                 {
