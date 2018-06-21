e73065ffb37c5eae353a0bc3d8531a9a562a1488
JeremyTCD <jeremytanchongdao@gmail.com>
2018-06-21 13:03:34 +0800

    Made changes to LineRange
    - Cleaned up comments
    - Fixed contains mistake, now considers EndLine being -1
    - Added CompareTo method
    - Fixed constructor guard clause

--- JeremyTCD.Markdig.Extensions/FlexiCode/LineRange.cs
+++ JeremyTCD.Markdig.Extensions/FlexiCode/LineRange.cs
@@ -10,8 +10,9 @@ namespace JeremyTCD.Markdig.Extensions.FlexiCode
         /// <summary>
         /// Creates an instance of type <see cref="LineRange"/>.
         /// </summary>
-        /// <param name="startLine">Must be larger than 0, <see cref="StartLine"/></param>
-        /// <param name="endLine">If not negative, must be greater than or equal to <paramref name="startLine"/></param>
+        /// <param name="startLine">Line that range begins at. Must be greater than 0, <see cref="StartLine"/></param>
+        /// <param name="endLine">Line that range ends at, -1 denotes a range that extends to infinity. If <paramref name="endLine"/> is not -1, it must be 
+        /// greater than or equal to <paramref name="startLine"/></param>
         public LineRange(int startLine, int endLine)
         {
             if(startLine < 1)
@@ -19,7 +20,7 @@ namespace JeremyTCD.Markdig.Extensions.FlexiCode
                 throw new ArgumentException(string.Format(Strings.ArgumentException_InvalidStartLine, startLine));
             }
 
-            if(endLine < startLine)
+            if(endLine != -1 && endLine < startLine)
             {
                 throw new ArgumentException(string.Format(Strings.ArgumentException_InvalidEndLine, endLine, startLine));
             }
@@ -29,14 +30,12 @@ namespace JeremyTCD.Markdig.Extensions.FlexiCode
         }
 
         /// <summary>
-        /// Gets the value specifying the line at which this line range begins. Lines numbers start from 1, this value cannot be
-        /// less than 1.
+        /// Gets the value specifying the line at which this line range begins.
         /// </summary>
         public int StartLine { get; }
 
         /// <summary>
-        /// Gets the value specifying the line at which this line range ends. If the value is negative, the range continues till the
-        /// end of the sequence of lines. If the value isn't negative, it must be greater than or equal to <see cref="StartLine"/>'s value. 
+        /// Gets the value specifying the line at which this line range ends.
         /// </summary>
         public int EndLine { get; }
 
@@ -47,7 +46,30 @@ namespace JeremyTCD.Markdig.Extensions.FlexiCode
         /// <returns>True if <paramref name="line"/> is within the range, otherwise false.</returns>
         public bool Contains(int line)
         {
-            return line >= StartLine && line <= EndLine;
+            return line >= StartLine && EndLine == -1 || line <= EndLine;
+        }
+
+        /// <summary>
+        /// Compares the current instance with <paramref name="lineRange"/> to determine the order in which they occur.
+        /// </summary>
+        /// <param name="lineRange"></param>
+        /// <returns>-1 if the current <see cref="LineRange"/> occurs before <paramref name="lineRange"/>. 0 if the <see cref="LineRange"/>s overlap.
+        /// 1 if the current <see cref="LineRange"/> occurs after <paramref name="lineRange"/>.</returns>
+        public int CompareTo(LineRange lineRange)
+        {
+            if(EndLine != -1 && EndLine < lineRange.StartLine)
+            {
+                return -1;
+            }
+            else if(StartLine <= lineRange.EndLine && EndLine == -1 || EndLine >= lineRange.StartLine)
+            {
+                return 0;
+            }
+            else
+            {
+                // StartLine > lineRange.EndLine
+                return 1;
+            }
         }
     }
 }
