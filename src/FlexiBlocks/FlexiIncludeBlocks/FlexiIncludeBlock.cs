f681d8b34b052c2bf33633ec92fa4e249d7f0f13
JeremyTCD <jeremytanchongdao@gmail.com>
2018-08-23 18:50:39 +0800

    Fleshed out FlexiIncludeBlock comments

--- src/FlexiBlocks/FlexiIncludeBlocks/FlexiIncludeBlock.cs
+++ src/FlexiBlocks/FlexiIncludeBlocks/FlexiIncludeBlock.cs
@@ -2,20 +2,39 @@
 
 namespace Jering.Markdig.Extensions.FlexiBlocks.FlexiIncludeBlocks
 {
+    /// <summary>
+    /// Represents a flexible include block.
+    /// </summary>
     public class FlexiIncludeBlock : LeafBlock
     {
+        /// <summary>
+        /// Creates a <see cref="FlexiIncludeBlock"/> instance.
+        /// </summary>
+        /// <param name="parser">The parser opening this instance.</param>
         public FlexiIncludeBlock(FlexiIncludeBlockParser parser) : base(parser)
         {
         }
 
+        /// <summary>
+        /// Gets or sets the source of the content that this FlexiIncludeBlock includes.
+        /// </summary>
         public string Source { get; set; }
 
+        /// <summary>
+        /// Gets or sets the line number in the source, of the last processed line. This value allows child 
+        /// FlexiIncludeBlocks to determine their line numbers in the source.
+        /// </summary>
         public int LineNumberOfLastProcessedLineInSource { get; set; }
 
+        /// <summary>
+        /// Gets or sets the source that this FlexiIncludeBlock occurs in.
+        /// </summary>
         public string ContainingSource { get; set; }
 
+        /// <summary>
+        /// Gets or sets this FlexiIncludeBlock's line number in the source that it occurs in.
+        /// </summary>
         public int LineNumberInContainingSource { get; set; }
-
 
         /// <summary>
         /// Number of open brackets in the JSON, used to determine when to stop parsing.
