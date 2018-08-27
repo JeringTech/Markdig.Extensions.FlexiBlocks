3e8c12accaa42fc237e2db29e5d5944f06ae009c
JeremyTCD <jeremytanchongdao@gmail.com>
2018-08-27 18:23:33 +0800

    Added JsonParserService

---
+++ src/FlexiBlocks/Shared/Parsing/JsonParserService.cs
@@ -0,0 +1,32 @@
+﻿using Markdig.Helpers;
+using Newtonsoft.Json;
+
+namespace Jering.Markdig.Extensions.FlexiBlocks
+{
+    /// <summary>
+    /// The default implementation of <see cref="IJsonParserService"/>.
+    /// </summary>
+    public class JsonParserService : IJsonParserService
+    {
+        private readonly JsonSerializer _jsonSerializer;
+
+        /// <summary>
+        /// Creates a <see cref="JsonParserService"/> instance.
+        /// </summary>
+        public JsonParserService()
+        {
+            _jsonSerializer = JsonSerializer.Create();
+        }
+
+        /// <inheritdoc />
+        public (int numLines, T result) Parse<T>(StringSlice stringSlice)
+        {
+            var textLineReader = new TextLineReader(stringSlice.Text, stringSlice.Start);
+            using (var jsonTextReader = new JsonTextReader(textLineReader))
+            {
+                T result = _jsonSerializer.Deserialize<T>(jsonTextReader);
+                return (textLineReader.LinesRead, result);
+            }
+        }
+    }
+}
