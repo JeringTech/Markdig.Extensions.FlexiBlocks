dcae3ec0399ae34e2e605e77dba30e6bae1793ca
JeremyTCD <jeremytanchongdao@gmail.com>
2018-09-04 18:36:54 +0800

    Added FlexiBlockRenderer

---
+++ src/FlexiBlocks/Shared/Rendering/FlexiBlockRenderer.cs
@@ -0,0 +1,48 @@
+﻿using Markdig.Renderers;
+using Markdig.Renderers.Html;
+using Markdig.Syntax;
+using System;
+
+namespace Jering.Markdig.Extensions.FlexiBlocks
+{
+    /// <summary>
+    /// <para>An abstraction for rendering FlexiBlocks.</para> 
+    /// 
+    /// <para>The primary functionality that this class provides is consistent exception handling, which facilitates easy locating of issues
+    /// in markdown.</para>
+    /// 
+    /// <para>Without this class's approach to exception handling,
+    /// exceptions thrown by a renderer will bubble up through Markdig and into the user facing application with
+    /// no information about the location of the offending markdown.</para>
+    /// 
+    /// This class ensures that any exception (besides FlexiBlocksExceptions) thrown by a renderer is wrapped in a FlexiBlockException with, at the very least, the
+    /// line and column of the offending markdown.
+    /// </summary>
+    /// <typeparam name="T">The type of FlexiBlock that this renderer renders.</typeparam>
+    public abstract class FlexiBlockRenderer<T> : HtmlObjectRenderer<T> where T : Block
+    {
+        /// <summary>
+        /// Renders a FlexiBlock as HTML.
+        /// </summary>
+        /// <param name="renderer">The renderer to write to.</param>
+        /// <param name="obj">The FlexiBlock to render.</param>
+        public abstract void WriteFlexiBlock(HtmlRenderer renderer, T obj);
+
+        /// <summary>
+        /// Renders a FlexiBlock as HTML.
+        /// </summary>
+        /// <param name="renderer">The renderer to write to.</param>
+        /// <param name="obj">The FlexiBlock to render.</param>
+        protected sealed override void Write(HtmlRenderer renderer, T obj)
+        {
+            try
+            {
+                WriteFlexiBlock(renderer, obj);
+            }
+            catch (Exception exception) when (!(exception is FlexiBlocksException))
+            {
+                throw new FlexiBlocksException(obj as Block, exception);
+            }
+        }
+    }
+}
---
+++ test/FlexiBlocks.Tests/Shared/FlexiBlockRendererUnitTests.cs
@@ -0,0 +1,60 @@
+﻿using Markdig.Parsers;
+using Markdig.Renderers;
+using Markdig.Syntax;
+using Moq;
+using System;
+using System.IO;
+using Xunit;
+
+namespace Jering.Markdig.Extensions.FlexiBlocks.Tests.Shared
+{
+    public class FlexiBlockRendererUnitTests
+    {
+        private readonly MockRepository _mockRepository = new MockRepository(MockBehavior.Default) { DefaultValue = DefaultValue.Mock };
+
+        [Fact]
+        public void Write_DoesNotInterfereWithFlexiBlocksExceptionsThrownByWriteFlexiBlock()
+        {
+            // Arrange
+            var dummyRenderer = new HtmlRenderer(new StringWriter());
+            var dummyBlock = new DummyBlock(null);
+            var dummyFlexiBlocksException = new FlexiBlocksException();
+            Mock<FlexiBlockRenderer<DummyBlock>> mockTestSubject = _mockRepository.Create<FlexiBlockRenderer<DummyBlock>>();
+            mockTestSubject.CallBase = true;
+            mockTestSubject.Setup(f => f.WriteFlexiBlock(dummyRenderer, dummyBlock)).Throws(dummyFlexiBlocksException);
+
+            // Act and assert
+            FlexiBlocksException result = Assert.Throws<FlexiBlocksException>(() => mockTestSubject.Object.Write(dummyRenderer, dummyBlock));
+            _mockRepository.VerifyAll();
+            Assert.Same(dummyFlexiBlocksException, result);
+            Assert.Null(result.InnerException);
+        }
+
+        [Fact]
+        public void Write_WrapsNonFlexiBlocksExceptionsThrownByWriteFlexiBlockInFlexiBlocksExceptions()
+        {
+            // Arrange
+            var dummyRenderer = new HtmlRenderer(new StringWriter());
+            var dummyBlock = new DummyBlock(null);
+            var dummyException = new ArgumentException(); // Arbitrary type
+            Mock<FlexiBlockRenderer<DummyBlock>> mockTestSubject = _mockRepository.Create<FlexiBlockRenderer<DummyBlock>>();
+            mockTestSubject.CallBase = true;
+            mockTestSubject.Setup(f => f.WriteFlexiBlock(dummyRenderer, dummyBlock)).Throws(dummyException);
+
+            // Act and assert
+            FlexiBlocksException result = Assert.Throws<FlexiBlocksException>(() => mockTestSubject.Object.Write(dummyRenderer, dummyBlock));
+            _mockRepository.VerifyAll();
+            Assert.Equal(@"The DummyBlock starting at line ""1"", column ""0"", is invalid:
+An unexpected exception occurred. Refer to the inner exception for more details.", 
+                result.Message, ignoreLineEndingDifferences: true);
+            Assert.Same(dummyException, result.InnerException);
+        }
+       
+        public class DummyBlock : Block
+        {
+            public DummyBlock(BlockParser parser) : base(parser)
+            {
+            }
+        }
+    }
+}
