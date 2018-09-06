696ff8eb490fa30a30045a5196d20696c0e6a6e0
JeremyTCD <jeremytanchongdao@gmail.com>
2018-09-06 16:08:22 +0800

    Added Benchmarks for FlexiAlertBlocks extension

---
+++ test/FlexiBlocks.Performance/Benchmarks.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using Markdig;
+
+namespace Jering.Markdig.Extensions.FlexiBlocks.Performance
+{
+    [MemoryDiagnoser]
+    public class Benchmarks
+    {
+        private MarkdownPipeline _pipeline;
+
+        [GlobalSetup(Target = nameof(FlexiAlertBlock_ParseAndRender))]
+        public void FlexiAlertBlock_ParseAndRender_Setup()
+        {
+            var pipelineBuilder = new MarkdownPipelineBuilder();
+            pipelineBuilder.UseFlexiAlertBlocks();
+            _pipeline = pipelineBuilder.Build();
+        }
+
+        [Benchmark]
+        public string FlexiAlertBlock_ParseAndRender()
+        {
+            return Markdown.ToHtml(@"! critical-warning
+! This is a critical warning.", _pipeline);
+        }
+    }
+}
---
+++ test/FlexiBlocks.Performance/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Jering.Markdig.Extensions.FlexiBlocks.Performance
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmarks>();
+        }
+    }
+}
