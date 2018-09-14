86c65cd1b4ef882b47f6e829960a00a3c2f68340
JeremyTCD <jeremytanchongdao@gmail.com>
2018-09-14 16:13:17 +0800

    Added JSON population tests for FlexiBlockOptions types

--- test/FlexiBlocks.Tests/FlexiAlertBlocks/FlexiAlertBlockOptionsUnitTests.cs
+++ test/FlexiBlocks.Tests/FlexiAlertBlocks/FlexiAlertBlockOptionsUnitTests.cs
@@ -1,12 +1,83 @@
 ﻿using Jering.Markdig.Extensions.FlexiBlocks.FlexiAlertBlocks;
+using Jering.Markdig.Extensions.FlexiBlocks.FlexiCodeBlocks;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Jering.Markdig.Extensions.FlexiBlocks.Tests.FlexiAlertBlocks
 {
     public class FlexiAlertBlockOptionsUnitTests
     {
+        [Theory]
+        [MemberData(nameof(FlexiAlertBlockOptions_CanBePopulated_Data))]
+        public void FlexiAlertBlockOptions_CanBePopulated(SerializableWrapper<FlexiAlertBlockOptions> dummyInitialOptionsWrapper,
+            SerializableWrapper<FlexiAlertBlockOptions> dummyExpectedOptionsWrapper,
+            string dummyJson)
+        {
+            // Act
+            JsonConvert.PopulateObject(dummyJson, dummyInitialOptionsWrapper.Value);
+
+            // Assert
+            FlexiAlertBlockOptions result = dummyInitialOptionsWrapper.Value;
+            FlexiAlertBlockOptions expectedResult = dummyExpectedOptionsWrapper.Value;
+            Assert.Equal(expectedResult.IconMarkup, result.IconMarkup);
+            Assert.Equal(expectedResult.ClassFormat, result.ClassFormat);
+            Assert.Equal(expectedResult.Class, result.Class);
+            Assert.Equal(expectedResult.ContentClass, result.ContentClass);
+            Assert.Equal(expectedResult.Type, result.Type);
+            Assert.Equal(expectedResult.Attributes, result.Attributes);
+        }
+
+        public static IEnumerable<object[]> FlexiAlertBlockOptions_CanBePopulated_Data()
+        {
+            const string dummyIconMarkup = "dummyIconMarkup";
+            const string dummyClassFormat = "dummy-{0}";
+            const string dummyContentClass = "dummyContentClass";
+            const string dummyType = "dummyType";
+            const string dummyAttribute1 = "dummyAttribute1";
+            const string dummyAttributeValue1 = "dummyAttributeValue1";
+            var dummyAttributes1 = new Dictionary<string, string> { { dummyAttribute1, dummyAttributeValue1 } };
+            const string dummyAttribute2 = "dummyAttribute2";
+            const string dummyAttributeValue2 = "dummyAttributeValue2";
+            var dummyAttributes2 = new Dictionary<string, string> { { dummyAttribute2, dummyAttributeValue2 } };
+
+            return new object[][]
+            {
+                // Populating FlexiAlertBlockOptions containing default values
+                new object[]
+                {
+                    new SerializableWrapper<FlexiAlertBlockOptions>(new FlexiAlertBlockOptions()),
+                    new SerializableWrapper<FlexiAlertBlockOptions>(new FlexiAlertBlockOptions(dummyIconMarkup,
+                        dummyClassFormat,
+                        dummyContentClass,
+                        dummyType,
+                        dummyAttributes1)),
+                    $@"{{
+    ""{nameof(FlexiAlertBlockOptions.IconMarkup)}"": ""{dummyIconMarkup}"",
+    ""{nameof(FlexiAlertBlockOptions.ClassFormat)}"": ""{dummyClassFormat}"",
+    ""{nameof(FlexiAlertBlockOptions.ContentClass)}"": ""{dummyContentClass}"",
+    ""{nameof(FlexiAlertBlockOptions.Type)}"": ""{dummyType}"",
+    ""{nameof(FlexiAlertBlockOptions.Attributes)}"": {{
+        ""{dummyAttribute1}"": ""{dummyAttributeValue1}""
+    }}
+}}"
+                },
+
+                // Populating FlexiAlertBlockOptions with an existing attributes collection (should be replaced instead of appended to)
+                new object[]
+                {
+                    new SerializableWrapper<FlexiAlertBlockOptions>(new FlexiAlertBlockOptions(attributes: dummyAttributes1)),
+                    new SerializableWrapper<FlexiAlertBlockOptions>(new FlexiAlertBlockOptions(attributes: dummyAttributes2)),
+                    $@"{{
+    ""{nameof(FlexiAlertBlockOptions.Attributes)}"": {{
+        ""{dummyAttribute2}"": ""{dummyAttributeValue2}""
+    }}
+}}"
+                }
+            };
+        }
+
         [Fact]
         public void ValidateAndPopulate_ThrowsFlexiBlocksExceptionIfClassFormatIsAnInvalidFormat()
         {
--- test/FlexiBlocks.Tests/FlexiCodeBlocks/FlexiCodeBlockOptionsUnitTests.cs
+++ test/FlexiBlocks.Tests/FlexiCodeBlocks/FlexiCodeBlockOptionsUnitTests.cs
@@ -8,6 +8,150 @@ namespace Jering.Markdig.Extensions.FlexiBlocks.Tests.FlexiCodeBlocks
 {
     public class FlexiCodeBlockOptionsUnitTests
     {
+        [Theory]
+        [MemberData(nameof(FlexiCodeBlockOptions_CanBePopulated_Data))]
+        public void FlexiCodeBlockOptions_CanBePopulated(SerializableWrapper<FlexiCodeBlockOptions> dummyInitialOptionsWrapper,
+            SerializableWrapper<FlexiCodeBlockOptions> dummyExpectedOptionsWrapper,
+            string dummyJson)
+        {
+            // Act
+            JsonConvert.PopulateObject(dummyJson, dummyInitialOptionsWrapper.Value);
+
+            // Assert
+            FlexiCodeBlockOptions result = dummyInitialOptionsWrapper.Value;
+            FlexiCodeBlockOptions expectedResult = dummyExpectedOptionsWrapper.Value;
+            Assert.Equal(expectedResult.CopyIconMarkup, result.CopyIconMarkup);
+            Assert.Equal(expectedResult.Title, result.Title);
+            Assert.Equal(expectedResult.Language, result.Language);
+            Assert.Equal(expectedResult.CodeClassFormat, result.CodeClassFormat);
+            Assert.Equal(expectedResult.CodeClass, result.CodeClass);
+            Assert.Equal(expectedResult.SyntaxHighlighter, result.SyntaxHighlighter);
+            Assert.Equal(expectedResult.HighlightJSClassPrefix, result.HighlightJSClassPrefix);
+            Assert.Equal(expectedResult.LineNumberRanges, result.LineNumberRanges);
+            Assert.Equal(expectedResult.HighlightLineRanges, result.HighlightLineRanges);
+            Assert.Equal(expectedResult.LineEmbellishmentClassesPrefix, result.LineEmbellishmentClassesPrefix);
+        }
+
+        public static IEnumerable<object[]> FlexiCodeBlockOptions_CanBePopulated_Data()
+        {
+            const string dummyCopyIconMarkup = "dummyCopyIconMarkup";
+            const string dummyTitle = "dummyTitle";
+            const string dummyLanguage = "dummyLanguage";
+            const string dummyCodeClassFormat = "dummy-{0}";
+            const SyntaxHighlighter dummySyntaxHighlighter = SyntaxHighlighter.HighlightJS;
+            const string dummyHighlightJSClassPrefix = "dummyHighlightJSClassPrefix";
+
+            var dummyLineNumberRange1 = new LineNumberRange(1, -1, 1);
+            var dummyLineNumberRanges1 = new List<LineNumberRange> { dummyLineNumberRange1 };
+            var dummyLineNumberRange2 = new LineNumberRange(10, 15, 8);
+            var dummyLineNumberRanges2 = new List<LineNumberRange> { dummyLineNumberRange2 };
+
+            var dummyHighlightLineRange1 = new LineRange(1, -1);
+            var dummyHighlightLineRanges1 = new List<LineRange> { dummyHighlightLineRange1 };
+            var dummyHighlightLineRange2 = new LineRange(10, 15);
+            var dummyHighlightLineRanges2 = new List<LineRange> { dummyHighlightLineRange2 };
+
+            const string dummyLineEmbellishementClassesPrefix = "dummyLineEmbellishementClassesPrefix";
+
+            const string dummyAttribute1 = "dummyAttribute1";
+            const string dummyAttributeValue1 = "dummyAttributeValue1";
+            var dummyAttributes1 = new Dictionary<string, string> { { dummyAttribute1, dummyAttributeValue1 } };
+            const string dummyAttribute2 = "dummyAttribute2";
+            const string dummyAttributeValue2 = "dummyAttributeValue2";
+            var dummyAttributes2 = new Dictionary<string, string> { { dummyAttribute2, dummyAttributeValue2 } };
+
+            return new object[][]
+            {
+                // Populating FlexiCodeBlockOptions containing default values
+                new object[]
+                {
+                    new SerializableWrapper<FlexiCodeBlockOptions>(new FlexiCodeBlockOptions()),
+                    new SerializableWrapper<FlexiCodeBlockOptions>(new FlexiCodeBlockOptions(dummyCopyIconMarkup,
+                        dummyTitle,
+                        dummyLanguage,
+                        dummyCodeClassFormat,
+                        dummySyntaxHighlighter,
+                        dummyHighlightJSClassPrefix,
+                        dummyLineNumberRanges1,
+                        dummyHighlightLineRanges1,
+                        dummyLineEmbellishementClassesPrefix,
+                        dummyAttributes1)),
+                    $@"{{
+    ""{nameof(FlexiCodeBlockOptions.CopyIconMarkup)}"": ""{dummyCopyIconMarkup}"",
+    ""{nameof(FlexiCodeBlockOptions.Title)}"": ""{dummyTitle}"",
+    ""{nameof(FlexiCodeBlockOptions.Language)}"": ""{dummyLanguage}"",
+    ""{nameof(FlexiCodeBlockOptions.CodeClassFormat)}"": ""{dummyCodeClassFormat}"",
+    ""{nameof(FlexiCodeBlockOptions.SyntaxHighlighter)}"": ""{dummySyntaxHighlighter}"",
+    ""{nameof(FlexiCodeBlockOptions.HighlightJSClassPrefix)}"": ""{dummyHighlightJSClassPrefix}"",
+    ""{nameof(FlexiCodeBlockOptions.LineNumberRanges)}"": [
+        {{ 
+            ""{nameof(LineRange.StartLineNumber)}"": {dummyLineNumberRange1.LineRange.StartLineNumber},
+            ""{nameof(LineRange.EndLineNumber)}"": {dummyLineNumberRange1.LineRange.EndLineNumber},
+            ""{nameof(LineNumberRange.FirstLineNumber)}"": {dummyLineNumberRange1.FirstLineNumber}
+        }}
+    ],
+    ""{nameof(FlexiCodeBlockOptions.HighlightLineRanges)}"": [
+        {{ 
+            ""{nameof(LineRange.StartLineNumber)}"": {dummyHighlightLineRange1.StartLineNumber},
+            ""{nameof(LineRange.EndLineNumber)}"": {dummyHighlightLineRange1.EndLineNumber}
+        }}
+    ],
+    ""{nameof(FlexiCodeBlockOptions.LineEmbellishmentClassesPrefix)}"": ""{dummyLineEmbellishementClassesPrefix}"",
+    ""{nameof(FlexiCodeBlockOptions.Attributes)}"": {{
+        ""{dummyAttribute1}"": ""{dummyAttributeValue1}""
+    }}
+}}"
+                },
+
+                // Populating FlexiCodeBlockOptions with an existing attributes collection (should be replaced instead of appended to)
+                new object[]
+                {
+                    new SerializableWrapper<FlexiCodeBlockOptions>(new FlexiCodeBlockOptions(lineNumberRanges: dummyLineNumberRanges1,
+                        highlightLineRanges: dummyHighlightLineRanges1, attributes: dummyAttributes1)),
+                    new SerializableWrapper<FlexiCodeBlockOptions>(new FlexiCodeBlockOptions(lineNumberRanges: dummyLineNumberRanges2,
+                        highlightLineRanges: dummyHighlightLineRanges2, attributes: dummyAttributes2)),
+                    $@"{{
+    ""{nameof(FlexiCodeBlockOptions.LineNumberRanges)}"": [
+        {{ 
+            ""{nameof(LineRange.StartLineNumber)}"": {dummyLineNumberRange2.LineRange.StartLineNumber},
+            ""{nameof(LineRange.EndLineNumber)}"": {dummyLineNumberRange2.LineRange.EndLineNumber},
+            ""{nameof(LineNumberRange.FirstLineNumber)}"": {dummyLineNumberRange2.FirstLineNumber}
+        }}
+    ],
+    ""{nameof(FlexiCodeBlockOptions.HighlightLineRanges)}"": [
+        {{ 
+            ""{nameof(LineRange.StartLineNumber)}"": {dummyHighlightLineRange2.StartLineNumber},
+            ""{nameof(LineRange.EndLineNumber)}"": {dummyHighlightLineRange2.EndLineNumber}
+        }}
+    ],
+    ""{nameof(FlexiCodeBlockOptions.Attributes)}"": {{
+        ""{dummyAttribute2}"": ""{dummyAttributeValue2}""
+    }}
+}}"
+                },
+
+                // Defaults for LineRanges in HighlightLineRanges work
+                new object[]
+                {
+                    new SerializableWrapper<FlexiCodeBlockOptions>(new FlexiCodeBlockOptions()),
+                    new SerializableWrapper<FlexiCodeBlockOptions>(new FlexiCodeBlockOptions(highlightLineRanges: dummyHighlightLineRanges1)),
+                    $@"{{
+    ""{nameof(FlexiCodeBlockOptions.HighlightLineRanges)}"": [{{ }}]
+}}"
+                },
+
+                // Defaults for LineNumberRanges in LineNumberRanges work
+                new object[]
+                {
+                    new SerializableWrapper<FlexiCodeBlockOptions>(new FlexiCodeBlockOptions()),
+                    new SerializableWrapper<FlexiCodeBlockOptions>(new FlexiCodeBlockOptions(lineNumberRanges: dummyLineNumberRanges1)),
+                    $@"{{
+    ""{nameof(FlexiCodeBlockOptions.LineNumberRanges)}"": [{{ }}]
+}}"
+                },
+            };
+        }
+
         [Fact]
         public void ValidateAndPopulate_ThrowsFlexiBlocksExceptionIfCodeLanguageClassFormatIsAnInvalidFormat()
         {
