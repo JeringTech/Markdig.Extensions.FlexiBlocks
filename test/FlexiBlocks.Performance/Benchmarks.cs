b2ab41f234cf73d3d4aea7f72a90a96bd637c351
JeremyTCD <jeremytanchongdao@gmail.com>
2018-09-14 18:28:25 +0800

    Added benchmark for FlexiCodeBlocksExtension

--- test/FlexiBlocks.Performance/Benchmarks.cs
+++ test/FlexiBlocks.Performance/Benchmarks.cs
@@ -1,5 +1,9 @@
 ﻿using BenchmarkDotNet.Attributes;
+using Jering.Markdig.Extensions.FlexiBlocks.FlexiAlertBlocks;
+using Jering.Markdig.Extensions.FlexiBlocks.FlexiCodeBlocks;
 using Markdig;
+using System;
+using System.IO;
 
 namespace Jering.Markdig.Extensions.FlexiBlocks.Performance
 {
@@ -12,15 +16,97 @@ namespace Jering.Markdig.Extensions.FlexiBlocks.Performance
         public void FlexiAlertBlock_ParseAndRender_Setup()
         {
             var pipelineBuilder = new MarkdownPipelineBuilder();
-            pipelineBuilder.UseFlexiAlertBlocks();
+            pipelineBuilder.
+                UseFlexiAlertBlocks().
+                UseFlexiOptionsBlocks();
             _pipeline = pipelineBuilder.Build();
+
+            WritePreview(nameof(FlexiAlertBlocksExtension), FlexiAlertBlock_ParseAndRender());
         }
 
         [Benchmark]
         public string FlexiAlertBlock_ParseAndRender()
         {
-            return Markdown.ToHtml(@"! critical-warning
+            return Markdown.ToHtml(@"@{ ""type"": ""info"" }
 ! This is a critical warning.", _pipeline);
+        }
+
+        [GlobalSetup(Target = nameof(FlexiCodeBlock_ParseAndRender))]
+        public void FlexiCodeBlock_ParseAndRender_Setup()
+        {
+            var pipelineBuilder = new MarkdownPipelineBuilder();
+            pipelineBuilder.
+                UseFlexiCodeBlocks().
+                UseFlexiOptionsBlocks();
+            _pipeline = pipelineBuilder.Build();
+
+            WritePreview(nameof(FlexiCodeBlocksExtension), FlexiCodeBlock_ParseAndRender());
+        }
+
+        [Benchmark]
+        public string FlexiCodeBlock_ParseAndRender()
+        {
+            return Markdown.ToHtml(@"@{
+    ""language"": ""csharp"",
+    ""lineNumberRanges"": [
+        {
+            ""startLineNumber"": 1,
+            ""endLineNumber"": 8,
+            ""firstLineNumber"": 1
+        },
+        {
+            ""startLineNumber"": 11,
+            ""endLineNumber"": -1,
+            ""firstLineNumber"": 32
+        }
+    ],
+    ""highlightLineRanges"": [
+        {
+            ""startLineNumber"": 3,
+            ""endLineNumber"": 7
+        },
+        {
+            ""startLineNumber"": 12,
+            ""endLineNumber"": 16
+        }
+    ]
+}
+```
+public class ExampleClass
+{
+    public string ExampleFunction1(string arg)
+    {
+        // Example comment
+        return arg + ""dummyString"";
+    }
+
+    // Some functions omitted for brevity
+    // ...
+
+    public string ExampleFunction3(string arg)
+    {
+        // Example comment
+        return arg + ""dummyString"";
+    }
+}
+```", _pipeline);
+        }
+
+        private void WritePreview(string extensionName, string preview)
+        {
+            Console.WriteLine($"// {extensionName} Benchmark Preview:");
+            Console.WriteLine("//");
+
+            using (var stringReader = new StringReader(preview))
+            {
+                string line;
+                while ((line = stringReader.ReadLine()) != null)
+                {
+                    Console.WriteLine($"// {line}");
+                }
+            }
+
+            Console.WriteLine();
         }
     }
 }
