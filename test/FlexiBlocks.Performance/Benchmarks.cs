31485c05d76900427aa51239ceaef66a218a1020
JeremyTCD <jeremytanchongdao@gmail.com>
2018-09-22 00:31:29 +0800

    Added benchmark for FlexiIncludeBlocks

--- test/FlexiBlocks.Performance/Benchmarks.cs
+++ test/FlexiBlocks.Performance/Benchmarks.cs
@@ -1,6 +1,7 @@
 ﻿using BenchmarkDotNet.Attributes;
 using Jering.Markdig.Extensions.FlexiBlocks.FlexiAlertBlocks;
 using Jering.Markdig.Extensions.FlexiBlocks.FlexiCodeBlocks;
+using Jering.Markdig.Extensions.FlexiBlocks.FlexiIncludeBlocks;
 using Markdig;
 using System;
 using System.IO;
@@ -90,6 +91,27 @@ public class ExampleClass
     }
 }
 ```", _pipeline);
+        }
+
+        [GlobalSetup(Target = nameof(FlexiIncludeBlock_ParseAndRender))]
+        public void FlexiIncludeBlock_ParseAndRender_Setup()
+        {
+            var pipelineBuilder = new MarkdownPipelineBuilder();
+            pipelineBuilder.
+                UseFlexiIncludeBlocks().
+                UseFlexiOptionsBlocks();
+            _pipeline = pipelineBuilder.Build();
+
+            WritePreview(nameof(FlexiIncludeBlocksExtension), FlexiIncludeBlock_ParseAndRender());
+        }
+
+        [Benchmark]
+        public string FlexiIncludeBlock_ParseAndRender()
+        {
+            return Markdown.ToHtml(@"+{
+    ""sourceUri"": ""https://raw.githubusercontent.com/JeremyTCD/Markdig.Extensions.FlexiBlocks/6998b1c27821d8393ad39beb54f782515c39d98b/test/FlexiBlocks.Tests/exampleInclude.js"",
+    ""clippings"":[{""startLineNumber"": 7, ""endDemarcationLineSubstring"": ""#endregion utility methods"", ""dedentLength"": 1, ""collapseRatio"": 0.5}]
+}", _pipeline);
         }
 
         private void WritePreview(string extensionName, string preview)
