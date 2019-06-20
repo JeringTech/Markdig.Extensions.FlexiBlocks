468d75c81d2ac60b30f0b0e2f269c6524031eb99
JeremyTCD <jeremytanchongdao@gmail.com>
2019-06-20 15:18:26 +0800

    Added shared types for models.

---
+++ src/FlexiBlocks/Shared/Models/LineRange.cs
@@ -0,0 +1,156 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
+
+namespace Jering.Markdig.Extensions.FlexiBlocks
+{
+    /// <summary>
+    /// Represents a range of lines.
+    /// </summary>
+    public class LineRange
+    {
+        /// <summary>
+        /// Default start line line number.
+        /// </summary>
+        protected const int _defaultStart = 1;
+
+        /// <summary>
+        /// Default end line line number.
+        /// </summary>
+        protected const int _defaultEnd = -1;
+
+        /// <summary>
+        /// Creates a <see cref="LineRange"/>.
+        /// </summary>
+        /// <param name="start">
+        /// <para>The line number of the <see cref="LineRange"/>'s start line.</para>
+        /// <para>If this value is <c>-n</c>, the start line is the nth last line. For example, if this value is <c>-2</c>, the start line is the 2nd last line.</para>
+        /// <para>This value must not be <c>0</c>.</para>
+        /// <para>Defaults to <c>1</c>.</para>
+        /// </param>
+        /// <param name="end">
+        /// <para>The line number of the <see cref="LineRange"/>'s end line.</para>
+        /// <para>If this value is <c>-n</c>, the end line is the nth last line. For example, if this value is <c>-2</c>, the end line is the 2nd last line.</para>
+        /// <para>This value must not be <c>0</c> or an integer representing a line before the start line.</para>
+        /// <para>Defaults to <c>-1</c>.</para>
+        /// </param>
+        /// <exception cref="OptionsException">Thrown if <paramref name="start"/> is 0.</exception>
+        /// <exception cref="OptionsException">Thrown if <paramref name="end"/> is 0.</exception>
+        /// <exception cref="OptionsException">Thrown if the end line is a line before the start line.</exception>
+        public LineRange(int start = _defaultStart, int end = _defaultEnd)
+        {
+            if (start == 0)
+            {
+                throw new OptionsException(nameof(Start),
+                    string.Format(Strings.OptionsException_Shared_InvalidValue, start));
+            }
+
+            if (end == 0)
+            {
+                throw new OptionsException(nameof(End),
+                    string.Format(Strings.OptionsException_Shared_InvalidValue, end));
+            }
+
+            if (end > 0 && start > 0 && end < start ||
+                end < 0 && start < 0 && end < start)
+            {
+                throw new OptionsException(string.Format(Strings.OptionsException_LineRange_EndLineBeStartLineOrALineAfterIt,
+                    start, end));
+            }
+
+            Start = start;
+            End = end;
+        }
+
+        /// <summary>
+        /// Gets the line number of the <see cref="LineRange"/>'s start line.
+        /// </summary>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate), DefaultValue(_defaultStart)]
+        public int Start { get; private set; }
+
+        /// <summary>
+        /// Gets the line number of the <see cref="LineRange"/>'s end line.
+        /// </summary>
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate), DefaultValue(_defaultEnd)]
+        public int End { get; private set; }
+
+        /// <summary>
+        /// <para>Gets the line numbers of the <see cref="LineRange"/>'s start and end lines normalized for a given number of lines.</para>
+        /// <para>Normalization converts negative values and <c>0</c> to positive values.</para>
+        /// </summary>
+        /// <param name="numLines">The number of lines to normalize for.</param>
+        /// <exception cref="OptionsException">Thrown if start and end cannot be normalized for the specified number of lines.</exception>
+        public (int normalizedStart, int normalizedEnd) GetNormalizedStartAndEnd(int numLines)
+        {
+            int normalizedStart = Start > 0 ? Start : numLines + Start + 1;
+            int normalizedEnd = End > 0 ? End : End == 0 ? numLines : numLines + End + 1;
+
+            if (normalizedStart < 1 || normalizedStart > numLines ||
+                normalizedEnd < normalizedStart || normalizedEnd > numLines)
+            {
+                throw new OptionsException(string.Format(Strings.OptionsException_LineRange_UnableToNormalize, ToString(),
+                    numLines, normalizedStart, normalizedEnd));
+            }
+
+            return (normalizedStart, normalizedEnd);
+        }
+
+        /// <summary>
+        /// Gets the position of the <see cref="LineRange"/> relative to a line in a sequence of lines.
+        /// </summary>
+        /// <param name="lineNumber">The line number of the line to find the <see cref="LineRange"/>'s position relative to.</param>
+        /// <param name="numLines">The number of lines in the sequence of lines.</param>
+        /// <returns>
+        /// -1 if the <see cref="LineRange"/> occurs before the line.
+        /// 0 if the <see cref="LineRange"/> contains the line.
+        /// 1 if the <see cref="LineRange"/> occurs after the line.
+        /// </returns>
+        /// <exception cref="OptionsException">Thrown if start and end cannot be normalized for the specified number of lines.</exception>
+        public int GetRelativePosition(int lineNumber, int numLines)
+        {
+            (int normalizedStart, int normalizedEnd) = GetNormalizedStartAndEnd(numLines);
+
+            if (lineNumber < normalizedStart)
+            {
+                return 1;
+            }
+
+            if (lineNumber > normalizedEnd)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Returns the string representation of the instance.
+        /// </summary>
+        public override string ToString()
+        {
+            return $"{nameof(Start)}: {Start}, {nameof(End)}: {End}";
+        }
+
+        /// <summary>
+        /// Checks for value equality between the <see cref="LineRange"/> and an object.
+        /// </summary>
+        /// <param name="obj">The object to check for value equality.</param>
+        /// <returns>True if the <see cref="LineRange"/>'s value is equal to the object's value, false otherwise.</returns>
+        public override bool Equals(object obj)
+        {
+            return obj is LineRange range &&
+                   Start == range.Start &&
+                   End == range.End;
+        }
+
+        /// <summary>
+        /// Returns the hash code for the object.
+        /// </summary>
+        /// <returns>The hash code for the object.</returns>
+        public override int GetHashCode()
+        {
+            int hashCode = -1676728671;
+            hashCode = hashCode * -1521134295 + Start.GetHashCode();
+            return hashCode * -1521134295 + End.GetHashCode();
+        }
+    }
+}
---
+++ test/FlexiBlocks/Shared/Models/LineRangeUnitTests.cs
@@ -0,0 +1,258 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Jering.Markdig.Extensions.FlexiBlocks.Tests
+{
+    public class LineRangeUnitTests
+    {
+        [Fact]
+        public void Constructor_ThrowsOptionsExceptionIfStartIs0()
+        {
+            // Arrange
+            const int dummyStart = 0;
+
+            // Act and assert
+            OptionsException result = Assert.Throws<OptionsException>(() => new LineRange(dummyStart));
+            Assert.Equal(string.Format(Strings.OptionsException_OptionsException_InvalidOption,
+                    nameof(LineRange.Start),
+                    string.Format(Strings.OptionsException_Shared_InvalidValue,
+                        dummyStart)),
+                result.Message);
+        }
+
+        [Fact]
+        public void Constructor_ThrowsOptionsExceptionIfEndIs0()
+        {
+            // Arrange
+            const int dummyEnd = 0;
+
+            // Act and assert
+            OptionsException result = Assert.Throws<OptionsException>(() => new LineRange(end: dummyEnd));
+            Assert.Equal(string.Format(Strings.OptionsException_OptionsException_InvalidOption,
+                    nameof(LineRange.End),
+                    string.Format(Strings.OptionsException_Shared_InvalidValue,
+                        dummyEnd)),
+                result.Message);
+        }
+
+        [Theory]
+        [MemberData(nameof(Constructor_ThrowsOptionsExceptionIfStartAndEndAreAnInvalidCombination_Data))]
+        public void Constructor_ThrowsOptionsExceptionIfStartAndEndAreAnInvalidCombination(int dummyStart, int dummyEnd)
+        {
+            // Act and assert
+            OptionsException result = Assert.Throws<OptionsException>(() => new LineRange(dummyStart, dummyEnd));
+            Assert.Equal(string.Format(Strings.OptionsException_LineRange_EndLineBeStartLineOrALineAfterIt, dummyStart, dummyEnd), result.Message);
+        }
+
+        public static IEnumerable<object[]> Constructor_ThrowsOptionsExceptionIfStartAndEndAreAnInvalidCombination_Data()
+        {
+            return new object[][]
+            {
+                // end > 0 && start > 0 && end < start
+                new object[]{ 124, 63 },
+                // end < 0 && start < 0 && end < start
+                new object[]{ -10, -11 }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(Constructor_DoesNotThrowAnyExceptionIfStartAndEndAreAValidCombination_Data))]
+        public void Constructor_DoesNotThrowAnyExceptionIfStartAndEndAreAValidCombination(int dummyStart, int dummyEnd)
+        {
+            // Act
+            var result = new LineRange(dummyStart, dummyEnd);
+
+            // Assert
+            Assert.Equal(dummyStart, result.Start);
+            Assert.Equal(dummyEnd, result.End);
+        }
+
+        public static IEnumerable<object[]> Constructor_DoesNotThrowAnyExceptionIfStartAndEndAreAValidCombination_Data()
+        {
+            return new object[][]
+            {
+                // start > 0 && end < 0
+                new object[]{ 1231, -12 },
+                // start < 0 && end > 0
+                new object[]{-42, 5},
+                // start > 0 && end > 0 && end > start
+                new object[]{ 10, 124 },
+                // start < 0 && end < 0 && end > start
+                new object[]{ -12, -3 },
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetNormalizedStartAndEnd_ThrowsOptionsExceptionIfNormalizedStartAndEndAreAnInvalidCombination_Data))]
+        public void GetNormalizedStartAndEnd_ThrowsOptionsExceptionIfNormalizedStartAndEndAreAnInvalidCombination(int dummyStart,
+            int dummyEnd,
+            int dummyNumLines,
+            int expectedNormalizedStart,
+            int expectedNormalizedEnd)
+        {
+            // Arrange
+            var testSubject = new LineRange(dummyStart, dummyEnd);
+
+            // Act and assert
+            OptionsException result = Assert.Throws<OptionsException>(() => testSubject.GetNormalizedStartAndEnd(dummyNumLines));
+            Assert.Equal(string.Format(Strings.OptionsException_LineRange_UnableToNormalize, testSubject, dummyNumLines, expectedNormalizedStart, expectedNormalizedEnd),
+                result.Message);
+        }
+
+        public static IEnumerable<object[]> GetNormalizedStartAndEnd_ThrowsOptionsExceptionIfNormalizedStartAndEndAreAnInvalidCombination_Data()
+        {
+            return new object[][]
+            {
+                // Normalized start < 1
+                new object[]{-4, 5, 3, 0, 5},
+                // Normalized start > num lines
+                new object[]{6, 7, 4, 6, 7},
+                // Normalized end < normalized start
+                new object[]{3, -5, 5, 3, 1},
+                // Normalized end > num lines
+                new object[]{3, 10, 5, 3, 10}
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetNormalizedStartAndEnd_GetsNormalizedStartAndEnd_Data))]
+        public void GetNormalizedStartAndEnd_GetsNormalizedStartAndEnd(int dummyStart,
+            int dummyEnd,
+            int dummyNumLines,
+            int expectedNormalizedStart,
+            int expectedNormalizedEnd)
+        {
+            // Arrange
+            var testSubject = new LineRange(dummyStart, dummyEnd);
+
+            // Act
+            (int normalizedStart, int normalizedEnd) = testSubject.GetNormalizedStartAndEnd(dummyNumLines);
+
+            // Assert
+            Assert.Equal(expectedNormalizedStart, normalizedStart);
+            Assert.Equal(expectedNormalizedEnd, normalizedEnd);
+        }
+
+        public static IEnumerable<object[]> GetNormalizedStartAndEnd_GetsNormalizedStartAndEnd_Data()
+        {
+            return new object[][]
+            {
+                // Normalized start == normalized end
+                new object[]{6, -5, 10, 6, 6},
+                // Normalized start < normalized end
+                new object[]{-4, -3, 8, 5, 6}
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetRelativePosition_GetsRelativePosition_Data))]
+        public void GetRelativePosition_GetsRelativePosition(int dummyStart,
+            int dummyEnd,
+            int dummyLineNumber,
+            int dummyNumLines,
+            int expectedResult)
+        {
+            // Arrange
+            var testSubject = new LineRange(dummyStart, dummyEnd);
+
+            // Act
+            int result = testSubject.GetRelativePosition(dummyLineNumber, dummyNumLines);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> GetRelativePosition_GetsRelativePosition_Data()
+        {
+            return new object[][]
+            {
+                // Before line
+                new object[]{ -9, 15, 21, 18, -1 },
+                // Contains line
+                new object[]{3, 123, 3, 291, 0},
+                new object[]{3, 123, 123, 291, 0},
+                new object[]{3, 123, 24, 291, 0},
+                // After line
+                new object[]{ -12, -3, 1, 15, 1}
+            };
+        }
+
+        [Fact]
+        public void ToString_ReturnsLineRangeAsString()
+        {
+            // Arrange
+            var lineRange = new LineRange(2, 4);
+
+            // Act
+            string result = lineRange.ToString();
+
+            // Assert
+            Assert.Equal("Start: 2, End: 4", result);
+        }
+
+        [Theory]
+        [MemberData(nameof(Equals_ReturnsTrueIfObjIsAnIdenticalLineRangeOtherwiseReturnsFalse_Data))]
+        public void Equals_ReturnsTrueIfObjIsAnIdenticalLineRangeOtherwiseReturnsFalse(LineRange dummyLineRange,
+            object dummyObj,
+            bool expectedResult)
+        {
+            // Act
+            bool result = dummyLineRange.Equals(dummyObj);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> Equals_ReturnsTrueIfObjIsAnIdenticalLineRangeOtherwiseReturnsFalse_Data()
+        {
+            const int dummyStart = 4; // Arbitrary
+            const int dummyEnd = 25; // Arbitrary
+
+            return new object[][]
+            {
+                new object[]{new LineRange(),
+                    "not a line range",
+                    false },
+                new object[]{new LineRange(),
+                    new LineRange(),
+                    true },
+                // False if the LineRanges differ in any way
+                new object[]{new LineRange(),
+                    new LineRange(dummyStart),
+                    false },
+                new object[]{new LineRange(),
+                    new LineRange(end: dummyEnd),
+                    false },
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetHashCode_ReturnsSameHashCodeForIdenticalLineRanges_Data))]
+        public void GetHashCode_ReturnsSameHashCodeForIdenticalLineRanges(LineRange dummyLineRange1,
+            LineRange dummyLineRange2,
+            bool identical)
+        {
+            // Act and assert
+            Assert.Equal(identical, dummyLineRange1.GetHashCode() == dummyLineRange2.GetHashCode());
+        }
+
+        public static IEnumerable<object[]> GetHashCode_ReturnsSameHashCodeForIdenticalLineRanges_Data()
+        {
+            const int dummyStart = 4; // Arbitrary
+            const int dummyEnd = 25; // Arbitrary
+
+            return new object[][]
+            {
+                new object[]{new LineRange(),
+                    new LineRange(),
+                    true },
+                new object[]{new LineRange(),
+                    new LineRange(dummyStart),
+                    false },
+                new object[]{new LineRange(),
+                    new LineRange(end: dummyEnd),
+                    false }
+            };
+        }
+    }
+}
