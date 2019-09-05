a31213f794bcf23b04dfbfa19bc59107a7d7b592
JeremyTCD <jeremytanchongdao@gmail.com>
2019-09-05 16:07:47 +0800

    Updated icons
    - URLs for material design icons changed.
    - Added custom icons.

---
+++ src/FlexiBlocks/Shared/Icons/CustomIcons.cs
@@ -0,0 +1,13 @@
+﻿namespace Jering.Markdig.Extensions.FlexiBlocks
+{
+    internal static class CustomIcons
+    {
+        // Based on the material design file copy icon
+        public const string CUSTOM_COPY = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"18px\" height=\"18px\" viewBox=\"0 0 18 18\"><path fill=\"none\" d=\"M0,0h18v18H0V0z\"/><path d=\"M12,1H2v13h2V3h8V1z M12,4l4,4v9H5V4H12z M11,9h4l-4-4V9z\"/></svg>";
+        // Based on the material design more vert icon
+        public const string CUSTOM_MORE_VERT = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"2px\" height=\"10px\" viewBox=\"0 0 2 10\"><rect shape-rendering=\"crispEdges\" width=\"2\" height=\"2\"/><rect shape-rendering=\"crispEdges\" y=\"4\" width=\"2\" height=\"2\"/><rect shape-rendering=\"crispEdges\" y=\"8\" width=\"2\" height=\"2\"/></svg>";
+        // Based on the material design format quote icon
+        public const string CUSTOM_QUOTE = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 14 10\"><path d=\"M13,0h-3L8,4v6h6V4h-3L13,0z M5,0H2L0,4v6h6V4H3L5,0z\"/></svg>";
+    }
+}
+
