eaf498a8807092878f46db5a1536ec94bb71ffd8
JeremyTCD <jeremytanchongdao@gmail.com>
2019-09-10 20:35:47 +0800

    Minor edits to comments in IProxyBlock.cs.

--- src/FlexiBlocks/Shared/Blocks/IProxyBlock.cs
+++ src/FlexiBlocks/Shared/Blocks/IProxyBlock.cs
@@ -5,10 +5,10 @@ namespace Jering.Markdig.Extensions.FlexiBlocks
 {
     /// <summary>
     /// <para>An abstraction for proxy blocks.</para>
-    /// <para>Many blocks can only be built after parsing. For example, we can only validate options for a <see cref="FlexiCodeBlock"/> when we know what code it will contain.</para>
-    /// <para>To build only after parsing, we use proxy blocks to accumulate data. These proxy blocks are then passed to block factories.</para>
-    /// <para>Apart from allowing us to build after parsing, this pattern also means less repetition in blocks. For example, instead of every fenced <see cref="IBlock"/> having an <see cref="IProxyFencedBlock.FenceChar"/> property (redundant after parsing),
-    /// fenced <see cref="IBlock"/>s are built using a shared proxy type with the required properties.</para>
+    /// <para>We can only build some blocks after parsing. For example, we can only validate options for a <see cref="FlexiCodeBlock"/> when we know what code contains.</para>
+    /// <para>To build only after parsing, we use proxy blocks to accumulate data. Proxy blocks are then passed to block factories.</para>
+    /// <para>Apart from allowing us to build after parsing, proxy blocks allow for less repeated logic in blocks. For example, instead of every fenced <see cref="IBlock"/> having an <see cref="IProxyFencedBlock.OpeningFenceCharCount"/> property (redundant after building),
+    /// fenced <see cref="IBlock"/>s are built using a shared proxy type with the required property.</para>
     /// </summary>
     public interface IProxyBlock : IBlock
     {
